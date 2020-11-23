efb1020cbc035bab5d84800e42f374f2edc9227c
Родион Кладов <kladov_1999@inbox.ru>
2020-11-24 00:12:18 +0500

    Coo,otybt

---
+++ kr/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kr
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int pv = int.Parse(PV.Text);
+            int r = int.Parse(R.Text);
+            int n = int.Parse(N.Text);
+            
+            int st = int.Parse(Stoimost.Text);
+
+            int procent=((st-pv)/100*n*r);
+            int RZ = (st - pv + procent)/84;
+            Ответ.Content = RZ.ToString();
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            N.Clear();
+            R.Clear();
+            PV.Clear();
+            Stoimost.Clear();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
